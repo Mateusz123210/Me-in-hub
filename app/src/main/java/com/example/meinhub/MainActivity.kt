df23cf884f9d44f5e37f5355464b2fb0687e5ddd
Mateusz123210 <mateusz.urbanczyk11@gmail.com>
2024-05-06 13:32:11 +0200

    Main screen done

--- app/src/main/java/com/example/meinhub/MainActivity.kt
+++ app/src/main/java/com/example/meinhub/MainActivity.kt
@@ -11,12 +11,13 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -25,7 +26,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
@@ -36,11 +36,11 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.meinhub.ui.theme.ButtonColor
 import com.example.meinhub.ui.theme.MeInHubTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -93,31 +93,88 @@ fun CenterAlignedTopAppBarExample() {
 fun ScrollContent(innerPadding: PaddingValues) {
     Column(modifier = Modifier
         .fillMaxWidth()
-        .padding(innerPadding)) {
+        .padding(innerPadding)
+        .verticalScroll(rememberScrollState())) {
         Row (modifier = Modifier.padding(12.dp)) {
             Text(text = "About application", color = MaterialTheme.colorScheme.secondary, fontSize = 32.sp,
                 fontFamily = FontFamily.SansSerif, fontWeight = FontWeight.Bold)
         }
         Row (modifier = Modifier.padding(8.dp)) {
-            val aboutText = "This app was created to show my books, my favourite footballers and cars, that I like."
+            val aboutText = "This app was created to show my books, my favourite footballers and cars, that I like." +
+                    " See more about me by clicking buttons below"
             Text(text = aboutText, color = MaterialTheme.colorScheme.secondary, fontSize = 20.sp,
                 fontFamily = FontFamily.SansSerif, fontWeight = FontWeight.Light)
         }
-        Row(modifier = Modifier.fillMaxWidth()) {
-            Column (modifier = Modifier.fillMaxWidth()) {
+
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp).padding(top=50.dp)) {
+            Column (modifier = Modifier.fillMaxWidth(), verticalArrangement = Arrangement.spacedBy(30.dp)) {
                 Row(modifier = Modifier.fillMaxWidth()) {
-                    Button(modifier = Modifier.fillMaxWidth(), onClick = { /* doSomething() */}) {
-                        Text("My books")
+                    Button(modifier = Modifier.fillMaxWidth(), onClick = { /* doSomething() */},
+                        colors = ButtonDefaults.buttonColors(containerColor = ButtonColor)
+                        ) {
+                        Column {
+                            Row (horizontalArrangement = Arrangement.Center) {
+                                Image(
+                                    modifier = Modifier.fillMaxWidth().requiredHeight(80.dp),
+                                    painter = painterResource(id = R.drawable.book),
+                                    contentDescription = "Book photo"
+                                )
+                            }
+                            Row {
+                                Text(text = "My books",
+                                    modifier = Modifier.fillMaxWidth(),
+                                    textAlign = TextAlign.Center,
+                                    color = Color.Black, fontSize = 20.sp,
+                                    fontFamily = FontFamily.SansSerif, fontWeight = FontWeight.Bold)
+                            }
+                        }
+
                     }
                 }
                 Row(modifier = Modifier.fillMaxWidth()) {
-                    Button(modifier = Modifier.fillMaxWidth(), onClick = { /* doSomething() */}) {
-                        Text("My favourite footballers")
+                    Button(modifier = Modifier.fillMaxWidth(), onClick = { /* doSomething() */},
+                        colors = ButtonDefaults.buttonColors(containerColor = ButtonColor)) {
+                        Column {
+                            Row (horizontalArrangement = Arrangement.Center) {
+                                Image(
+                                    modifier = Modifier.fillMaxWidth().requiredHeight(80.dp),
+                                    painter = painterResource(id = R.drawable.footballer),
+                                    contentDescription = "Footballer photo"
+                                )
+                            }
+                            Row {
+                                Text(text = "My favourite footballers",
+                                    modifier = Modifier.fillMaxWidth(),
+                                    textAlign = TextAlign.Center,
+                                    color = Color.Black, fontSize = 20.sp,
+                                    fontFamily = FontFamily.SansSerif, fontWeight = FontWeight.Bold)
+                            }
+                        }
+
                     }
                 }
                 Row(modifier = Modifier.fillMaxWidth()) {
-                    Button(modifier = Modifier.fillMaxWidth(), onClick = { /* doSomething() */}) {
-                        Text("Cars, that I like")
+                    Button(modifier = Modifier.fillMaxWidth(), onClick = { /* doSomething() */},
+                        colors = ButtonDefaults.buttonColors(containerColor = ButtonColor)) {
+                        Column {
+                            Row (horizontalArrangement = Arrangement.Center) {
+                                Image(
+                                    modifier = Modifier.fillMaxWidth().requiredHeight(80.dp),
+                                    painter = painterResource(id = R.drawable.car),
+                                    contentDescription = "Car photo"
+                                )
+                            }
+                            Row {
+                                Text(text = "Cars, that I like",
+                                    modifier = Modifier.fillMaxWidth(),
+                                    textAlign = TextAlign.Center,
+                                    color = Color.Black, fontSize = 20.sp,
+                                    fontFamily = FontFamily.SansSerif, fontWeight = FontWeight.Bold)
+                            }
+                        }
+
                     }
                 }
             }
